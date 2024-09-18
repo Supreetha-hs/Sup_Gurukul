48108a8a52850a92dfef8ae148f12eee23b832c4
Supreetha-hs <supreetha.h_s@nokia.com>
2024-09-18 15:02:31 +0530

    Assignment on Variables, tuples & disctionaries

---
+++ Dictionary.py
@@ -0,0 +1,36 @@
+#1.Create a dictionary where the keys are student names and the values are their grades.
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# print(students)
+
+#2 How do you access Bob's grade from the dictionary students = {"Alice": 85, "Bob": 90, "Charlie": 78}?
+# Grade_of_Bob = students["Bob"]
+# print(Grade_of_Bob)
+
+#3 Add a new student "David": 92 to the dictionary students = {"Alice": 85, "Bob": 90, "Charlie": 78} and remove "Charlie" from the dictionary
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# students.update ({"David" : 92})
+# del students["Charlie"]
+# print(students)
+#OR#
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# students["David"] = 92
+# del students["Charlie"]
+# print(students)
+
+#4 Write Python program to update Bob's grade to 95 in the dictionary students = {"Alice": 85, "Bob": 90, "Charlie": 78}.
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# students["Bob"] = 95
+# print(students)
+
+#5 Write a Python program to check if "Alice" is a key in the dictionary students = {"Alice": 85, "Bob": 90, "Charlie": 78}.
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# if "Alice" in students:
+#     print("Alice is a key")
+# else:
+#     print("Key Alice is not present")
+
+#6 Write a Python program to find the number of key-value pairs in the dictionary students = {"Alice": 85, "Bob": 90, "Charlie": 78}.
+# students={"Alice" : 85, "Bob" : 90, "Charlie" : 78}
+# Length=len(students)
+# print(Length)
+
