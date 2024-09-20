b4d36ae7835ad27b21c8e1acd435b063daeb11f6
Supreetha-hs <supreetha.h_s@nokia.com>
2024-09-20 22:33:52 +0530

    Assignment_Day-1

---
+++ Operators.py
@@ -0,0 +1,50 @@
+# #Question 1: Write a Python program that takes two numbers as input from the user and performs the following operations:
+# #Addition,Subtraction.Multiplication,Division, Modulus & Display the results for each operation.
+# a=int(input("Enter a value for: a"))
+# b=int(input("Enter a value for: b"))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+#################
+
+# #Question 2: Write a Python program that checks whether a given number is even or odd using the modulus operator.
+# Given_number = int(input("Enter a number to check whether it is even or odd"))
+# if Given_number%2 ==0:
+#     print("The given input is an even number")
+# else:
+#     print("The given input is an odd number")
+
+###############
+
+# #Question 3: Write a Python program that takes a number as input from the user and checks if it is positive, negative, or zero.
+# Given_number = float(input("Enter a number : "))
+# if Given_number>0:
+#     print("The number is positive.")
+# elif Given_number<0:
+#     print("The number is negative")
+# else:
+#     print("The number is zero")
+
+# #Question 4: Write a Python program that calculates the grade of a student based on the marks entered by the user. The grading scale is as follows:
+# #90 and above: A
+# #80 - 89: B
+# #70 - 79: C
+# #60 - 69: D
+# #Below 60: F
+# Marks = float(input("Enter the marks:"))
+# if Marks >= 90:
+#     print("The grade is A")
+# elif Marks >=80 and Marks<90:
+#     print("The grade is B")
+# elif Marks >= 70 and Marks<80:
+#     print("The grade is C")
+# elif Marks >=60 and Marks<70:
+#     print("The grade is B")
+# else:
+#     print("The garde is F")
+
+
+
